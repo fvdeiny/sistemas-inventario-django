9650aae3a10d73da5a7d5924e31b4c3d7f30fa60
Deiny Alberto Fuentes Vargas <serviphone217@gmail.com>
2026-06-29 20:11:02 -0500

    auth: implementa autenticacion por token de forma global para la API

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken', # sistema de llave de seguridad 
     'inventory',
 ]
 
@@ -122,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Configuración global de Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token  # <-- IMPORTANTE
 from inventory.views import lista_productos ,registrar_movimiento, api_lista_productos  # importamos las vista
 
 
@@ -24,4 +10,5 @@ urlpatterns = [
     path('productos/', lista_productos, name = 'lista_productos' ), # Nueva ruta 
     path('productos/movimiento/', registrar_movimiento, name='registrar_movimiento'),
     path('api/productos/', api_lista_productos, name='api_lista_productos'),
+    path('api/token/', obtain_auth_token, name='api_token'),  # <-- NUEVO ENDPOINT DE SEGURIDAD
 ]
